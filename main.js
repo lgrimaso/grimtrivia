5affb7943c272152b8524dd27b11a6f4189f30d7
lgrimaso <lgrimaso@mtu.edu>
2024-04-18 22:41:44 -0400

    Added quizzes until 2036, they will reveal every day

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ function correct(button) {
 function wrong(button) {
     button.classList.add("wrong");
 }
+
+function reveal() {
+    let today = new Date();
+    const cards = document.querySelectorAll('div.quiz-cards');
+    for (let i = 0; i < cards.length; i++){
+        let item_date = new Date(cards[i].getAttribute("date"));
+
+        if (item_date > today) {
+            cards[i].style.display = 'none';
+        }
+    }
+}
