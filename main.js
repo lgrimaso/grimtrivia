2c8ce855d6c756589e872091fbf1005653cfa689
lgrimaso <lgrimaso@mtu.edu>
2024-04-25 19:54:36 -0400

    Added Confetti

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 function correct(button) {
     button.classList.add("correct");
+    var end = Date.now() + 500;
+
+    // go Buckeyes!
+    var colors = ['#007910', '#ffffff'];
+    
+    (function frame() {
+      confetti({
+        particleCount: 2,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 },
+        colors: colors
+      });
+      confetti({
+        particleCount: 2,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1 },
+        colors: colors
+      });
+    
+      if (Date.now() < end) {
+        requestAnimationFrame(frame);
+      }
+    }());
 }
 
 function wrong(button) {
